0508f7b2b9a05c9eddf5ee252f4775f6d7d51035
samueltobyknight <56262589+samueltobyknight@users.noreply.github.com>
2021-09-08 15:08:27 +0100

    page logic handler

--- cmd/segment.go
+++ cmd/segment.go
@@ -3,8 +3,10 @@ package cmd
 // go get ./...
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
@@ -12,17 +14,68 @@ import (
 )
 
 type Event struct {
-	Version    float64                `json:"version"`
-	Event      string                 `json:"type"`
-	UserId     string                 `json:"UserId"`
-	Properties map[string]interface{} `json:"properties"`
-	Timestamp  string                 `json:"timestamp"`
+	AnonymousID string `json:"anonymousId"`
+	Context     struct {
+		IP      string `json:"ip"`
+		Library struct {
+			Name    string `json:"name"`
+			Version string `json:"version"`
+		} `json:"library"`
+		Locale string `json:"locale"`
+		Page   struct {
+			Path     string `json:"path"`
+			Referrer string `json:"referrer"`
+			Search   string `json:"search"`
+			Title    string `json:"title"`
+			URL      string `json:"url"`
+		} `json:"page"`
+		SignalDeviceID string `json:"signalDeviceId"`
+		UserAgent      string `json:"userAgent"`
+	} `json:"context"`
+	Integrations struct {
+	} `json:"integrations"`
+	MessageID         string    `json:"messageId"`
+	OriginalTimestamp time.Time `json:"originalTimestamp"`
+	Properties        struct {
+		App struct {
+			Version string `json:"version"`
+		} `json:"app"`
+		Environment string `json:"environment"`
+		Path        string `json:"path"`
+		Referrer    string `json:"referrer"`
+		Search      string `json:"search"`
+		Title       string `json:"title"`
+		URL         string `json:"url"`
+		UserAgent   string `json:"userAgent"`
+		UserID      string `json:"userId"`
+		WorkspaceID string `json:"workspaceId"`
+	} `json:"properties"`
+	ReceivedAt time.Time `json:"receivedAt"`
+	SentAt     time.Time `json:"sentAt"`
+	Timestamp  time.Time `json:"timestamp"`
+	Type       string    `json:"type"`
+	UserID     string    `json:"userId"`
 }
 
 var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
+	totalPageCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "total_page_views",
+		Help: "The total number of page views recorded by segment.io",
+	})
+
+	searchPageCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "total_search_page_views",
+		Help: "The total number of search page views recorded by segment.io",
+	})
+
+	insightsPageCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "total_insights_page_views",
+		Help: "The total number of insight page views recorded by segment.io",
+	})
+
+	dashboardPageCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "total_dashboard_page_views",
+		Help: "The total number of dashboard page views recorded by segment.io",
 	})
 )
 
@@ -31,16 +84,29 @@ func segmentHandler(c *gin.Context) {
 	if handleError(c, "unable to read request", err) {
 		return // exit
 	}
-	log.Println(string(payload))
 
 	var e Event
 	err = json.Unmarshal(payload, &e)
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("%+v", e)
 
-	opsProcessed.Inc()
-
-	c.JSON(200, "success")
+	switch e.Type {
+	case "page":
+		totalPageCounter.Inc()
+		if strings.Contains(e.Properties.Path, "search") {
+			searchPageCounter.Inc()
+			if strings.Contains(e.Properties.Path, "insights") {
+				insightsPageCounter.Inc()
+			}
+		}
+		if strings.Contains(e.Properties.Path, "dashboards") {
+			dashboardPageCounter.Inc()
+		}
+		c.JSON(200, "success")
+	default:
+		message := fmt.Sprintf("Event %s ignored\n", e.Type)
+		c.JSON(200, message)
+		return
+	}
 }
